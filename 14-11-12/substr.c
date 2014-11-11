0d4ac54b3dd106fe465544d36fc50fd3705c273c
Kiril Smirenko <ksmirenko@mail.ru>
2014-11-11 19:56:10 +0300

    Removed debug code, done Substring

--- 14-11-12/substr.c
+++ 14-11-12/substr.c
@@ -8,41 +8,47 @@
 
 const int MAX_LENGTH = 50;
 
-int firstOcc(char *s, char *subs) {
+int firstOcc(char s[], char subs[]) {
     int i = 0, j = 0;
-    while (s[i] != '\0') {
-        while ((s[i] != '\0') && (s[i] != subs[0])) {
+    while (s[i] != '\n') {
+        while ((s[i] != '\n') && (s[i] != subs[0])) {
              ++i;
         }
-        if (s[i] == '\0') {
+        if (s[i] == '\n') {
             return -1;
         }
         
         j = 0;
-        while ((s[i + j] != '\0') && (subs[j] != '\0') &&
+        while ((s[i + j] != '\n') && (subs[j] != '\n') &&
             (s[i + j] == subs[j])) {
             ++j;
         }
         
-        if (subs[j] == '\0') {
+        if (subs[j] == '\n') {
             return i;
         }
-        if (s[i] == '\0') {
+        if (s[i] == '\n') {
             return -1;
         }
+        
+        ++i;
     }
+    return -1;
 }
 
 int main() {
-    char *str, *substr;
+    char str[MAX_LENGTH], substr[MAX_LENGTH];
     
     printf("Type a string (no more than %d characters: ", MAX_LENGTH);
     fgets(str, MAX_LENGTH, stdin);
     printf("Type the substring (no more than %d characters: ", MAX_LENGTH);
     fgets(substr, MAX_LENGTH, stdin);
     
+    int result = firstOcc(str, substr);
     
-    
-    printf("Not found\n");
+    if (result > -1)
+        printf("Index of first occurence: %d\n", result);
+    else
+        printf("Not found\n");
     return 0;
 }
--- 14-11-12/tester.c
+++ 14-11-12/tester.c
@@ -6,8 +6,6 @@
 #include <stdio.h>
 #include "singleList.h"
 
-const int DEBUG = 1;
-
 int main() {
     char curCommand = 0;
     int arg;
@@ -17,27 +15,19 @@ int main() {
         switch (curCommand) {
             case 'a':
                 scanf("%d", &arg);
-                if (DEBUG)
-                    printf("ADD %d\n", arg);
                 list_add(arg);
                 break;
             case 'p':
-                if (DEBUG)
-                    printf("PRINT\n");
                 list_print();
                 break;
             case 'r':
                 scanf("%d", &arg);
-                if (DEBUG)
-                    printf("REMOVE %d\n", arg);
                 list_removeByValue(arg);
                 break;
             default:
                 break;
         }
     }
-    if (DEBUG)
-        printf("QUIT\n");
     list_init();
     return 0;
 }
