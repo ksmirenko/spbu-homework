604edf007dfa328b559671bc1fc338a13c64f93c
Kirill Smirenko <cyrussmith1865@gmail.com>
2015-03-20 00:51:54 +0300

    Сделано всё, кроме 29 и 30.

--- 15-03-16/part1.fs
+++ 15-03-16/part1.fs
@@ -1,108 +1,158 @@
 ﻿// Задачи 20-25 от 16 марта
 // Автор: Кирилл Смиренко, группа 171
-// Расчётное время выполнения: 3 часа (c 18:30)
-// Действительное время выполнения: 3 часа +...
+// Расчётное время выполнения: 3 часа
+// Действительное время выполнения: 5 часов
 
 // 20. Интерфейс ориентированного графа, пригодный для решения дальнейших задач
-type IGraph =
+type IGraph<'V> =
   interface
+    // возвращает количество вершин
+    abstract GetSize      : unit -> int
+    // возвращает значение, "написанное" на вершине с данным индексом (с нуля)
+    abstract GetValue     : int -> 'V
+    // отвечает, есть ли прямое ребро из одной вершины в другую
     abstract HasEdge      : int -> int -> bool
-    // для задания 23
-    abstract GetOutEdges  : int -> int list // TODO: remove
-    // для задания 24
-    abstract GetInEdges   : int -> int list
-    // решил, что лучше сделать различные реализации заданий 23-24,
-    // чтобы учитывать конкретную реализацию графа
+    // возвращает список вершин, в которые есть прямое ребро из данной
+    abstract GetOutEdges  : int -> int list
+    // печатает граф на экран
+    abstract Print        : unit -> unit
   end
 
 // 21. Реализация интерфейса графа с матрицей смежности
-type AdjMatrixGraph (s : int, e : list<int * int>) =
+type AdjMatrixGraph<'V> (vert : 'V [], edges : list<int * int>) =
   class
-    let size = s
-    let matrix = Array2D.create size size false
+    let v = vert
+    let size = v.Length
+    let e = Array2D.create size size false
 
     do
       try
-        for (u, v) in e do
-          Array2D.set matrix u v true
+        for (v1, v2) in edges do
+          Array2D.set e v1 v2 true
       with
         | _ -> printfn "Invalid input data!"
 
-    interface IGraph with
+    interface IGraph<'V> with
+      member this.GetSize () = size
+      member this.GetValue i =
+        if i < size then
+          Array.get v i
+        else
+          failwith "Index out of range!"
       member this.HasEdge v1 v2 =
-        if ((v1 < size) && (v2 < size)) then
-          Array2D.get matrix v1 v2
+        if (v1 < size) && (v2 < size) then
+          Array2D.get e v1 v2
         else
           false
-      member this.GetOutEdges v = // TODO
-        if (v < size) then
-          let rec oe i l =
-            if i < 0 then
+      member this.GetOutEdges i =
+        if i < size then
+          let rec oe j l =
+            if j < 0 then
               l
-            elif Array2D.get matrix v i then
-              oe (i - 1) (i::l)
+            elif Array2D.get e i j then
+              oe (j - 1) (j::l)
             else
-              oe (i - 1) l
+              oe (j - 1) l
           oe (size - 1) []
         else
-          []
-      member this.GetInEdges v = // TODO
-        if (v < size) then
-          let rec oe i l =
-            if i < 0 then
-              l
-            elif Array2D.get matrix i v then
-              oe (i - 1) (i::l)
-            else
-              oe (i - 1) l
-          oe (size - 1) []
-        else
-          []
+          failwith "Index out of range!"
+      member this.Print () =
+        printfn "Вершины: %A" v
+        printfn "Рёбра:\n%A" e
   end
 
 // 22. Реализация интерфейса графа со списками смежности
-type AdjListGraph (s, e : list<int * int>) =
+type AdjListGraph<'V> (vert : 'V [], edges : list<int * int>) =
   class
-    let size = s
-    let edges : int list [] = Array.create size List.empty
+    let v = vert
+    let size = v.Length
+    let e : int list [] = Array.create size List.empty
 
     do
-      for (u, v) in e do
-        Array.set edges u (v::(Array.get edges u))
+      for (v1, v2) in edges do
+        Array.set e v1 (v2::(Array.get e v1))
 
-    interface IGraph with
-      member this.HasEdge u v =
-        if (u < size) then
-          List.exists (fun v' -> v = v') (Array.get edges u)
+    interface IGraph<'V> with
+      member this.GetSize () = size
+      member this.GetValue i =
+        if i < size then
+          Array.get v i
+        else
+          failwith "Index out of range!"
+      member this.HasEdge v1 v2 =
+        if (v1 < size) && (v2 < size) then
+          List.exists ((=) v2) (Array.get e v1)
         else
           false
-      member this.GetOutEdges v =
-        Array.get edges v // TODO
-      member this.GetInEdges v =
-        [] // TODO
-       //Array.foldBack (List.foldBack ) edges []
+      member this.GetOutEdges i =
+        if i < size then
+          Array.get e i
+        else
+          failwith "Index out of range!"
+      member this.Print () =
+        printfn "Вершины: %A" v
+        printfn "Рёбра: %A" e
   end
 
 // 23. По вершине вернуть список вершин, доступных из нее
+// принимается индекс вершины, возвращается список "значений" вершин
+let spreadOut (graph : IGraph<'V>) v =
+  let s = graph.GetSize()
+  if v >= s then
+    failwith "Index out of range!"
+  let visited = Array.create s false
+  let rec dfs v =
+    if not (Array.get visited v) then
+      Array.set visited v true
+      for i in graph.GetOutEdges v do
+        dfs i
+    else ()
+  dfs v
+  List.map graph.GetValue (List.filter (Array.get visited) [0 .. (s-1)])
 
 // 24. По вершине вернуть список вершин, из которых она доступна
+let spreadIn (graph : IGraph<'V>) v =
+  let s = graph.GetSize()
+  if v >= s then
+    failwith "Index out of range!"
+  let search u =
+    let visited = Array.create s false
+    let rec dfs u =
+      if u = v then
+        true
+      elif not (Array.get visited u) then
+        Array.set visited u true
+        List.fold (fun acc u -> acc || (dfs u)) false (graph.GetOutEdges u)
+      else false
+    dfs u
+  List.map graph.GetValue (List.filter search [0 .. (s-1)])
 
 // 25. Интерфейс полиморфного помеченного графа, использующий интерфейс из 20 задачи
-type IMarkedGraph<'K, 'M> =
+type IMarkedGraph<'V, 'M> =
   interface
-    inherit IGraph
+    inherit IGraph<'V>
     // метод HasEdge из IGraph здесь будет принимать в качестве аргументов
-    // индексы проверяемых вершин, а HasEdgeK - "обозначения вершин" полиморфного типа
+    // индексы проверяемых вершин, а HasEdgeByVal - "названия" вершин
 
-    abstract HasEdgeK  : 'K -> 'K -> bool
+    abstract HasEdgeByVal  : 'V -> 'V -> bool
 
-    abstract GetMark  : 'K -> 'K -> 'M
+    abstract GetMark  : 'V -> 'V -> 'M
   end
 
 [<EntryPoint>]
 let main argv =
-  let graph1 = new AdjMatrixGraph(4, [(0, 1); (0, 3)])
-  let graph2 = new AdjListGraph(4, [(0, 1); (0, 3)])
-  printfn "21. Граф с матрицей смежности:\n"
-  printfn "22. Граф со списками смежности:\n"
+  let g1 =
+    new AdjMatrixGraph<int>([|1; 2; 3; 4|],
+      [(0, 1); (0, 3); (1, 0); (3, 0)])
+  let g2 =
+    new AdjListGraph<char>([|'A'; 'B'; 'C'; 'D'; 'E'; 'F' |],
+      [(0, 1); (1, 2); (2, 3); (4, 0); (1, 5); (4, 5); (5, 0)])
+  printfn "21. Граф с матрицей смежности:"
+  (g1 :> IGraph<int>).Print()
+  printfn "\n22. Граф со списками смежности:"
+  (g2 :> IGraph<char>).Print()
+  printfn "\n23. Cписок вершин, доступных из вершины А второго графа:\n%A"
+    (spreadOut g2 0)
+  printfn "\n24. Cписок вершин, из которых доступна из вершина А второго графа:\n%A"
+    (spreadIn g2 0)
   0
--- 15-03-16/part3.fs
+++ 15-03-16/part3.fs
@@ -1,4 +1,4 @@
-﻿// Задачи 27-29 от 16 марта
+﻿// Задачи 27-29 Zот 16 марта
 // Автор: Кирилл Смиренко, группа 171
 // Расчётное время выполнения: 2 часа (с 2:00)
 // Действительное время выполнения: 
@@ -6,6 +6,8 @@
 // 27. Интерфейс для полиморфного списка
 type ICList<'A when 'A : equality> =
   interface
+    abstract HeadValue   : unit          -> Option<'A>
+    abstract Length      : unit          -> int
     abstract AddHead     : 'A            -> ICList<'A>
     abstract AddTail     : 'A            -> ICList<'A>
     abstract AddBefore   : 'A            -> int         -> ICList<'A>
@@ -18,55 +20,135 @@ type ICList<'A when 'A : equality> =
 
 type Node<'A> =
   | Empty
-  | N of 'A * Option<Node<'A>>
+  | N of 'A * Node<'A>
 
 // 28. Реализация интерфейса списка на АТД
-type CListAdt<'A when 'A : equality>(value' : 'A, next' : Option<CListAdt<'A>>) =
+type CListAdt<'A when 'A : equality>(h : Node<'A>, s : int) =
   class
-    let value = value'
-    let next = next'
+    let head = h
+    let length = s
 
-    new() = CListAdt<'A>(Unchecked.defaultof<'A>, None)
+    let rec tail (l : Node<'A>) =
+      match l with
+      | Empty -> Empty
+      | N(_, Empty) -> l
+      | N(_, n) -> tail n
 
-    member this.GetValue =
-      value
-    member this.GetNext =
-      next
+    new() = CListAdt<'A>(Empty, 0)
+    new(v : 'A) = CListAdt<'A>(N(v, Empty), 1)
+
+    member this.HeadNode = head
 
     interface ICList<'A> with
+      member this.HeadValue () =
+        match head with
+        | Empty -> None
+        | N(v, _) -> Some v
+      member this.Length () =
+        length
       member this.AddHead v =
-        new CListAdt<'A>(v, Some this) :> ICList<'A>
+        new CListAdt<'A>(N(v, head), length + 1) :> ICList<'A>
       member this.AddTail v =
-        this :> ICList<'A>
+        let rec at n v =
+          match n with
+          | Empty -> N(v, Empty)
+          | N(v', n') -> N(v', at n' v)
+        CListAdt(at head v, length + 1) :> ICList<'A>
       member this.AddBefore v i =
-        this :> ICList<'A>
+        if i > length then
+          (this :> ICList<'A>).AddTail v
+        else
+          let rec ab node v index =
+            match node with
+            | Empty -> N(v, Empty)
+            | N(vl, nxt) ->
+              match compare index i with
+              | c when c < 0 -> N(vl, ab nxt v (index + 1))
+              | _ -> N(v, node)
+          CListAdt(ab head v 0, length + 1) :> ICList<'A>
       member this.RemoveHead () =
-        this :> ICList<'A>
+        match head with
+        | Empty -> this :> ICList<'A>
+        | N(vl, nxt) -> CListAdt<'A>(nxt, length - 1) :> ICList<'A>
       member this.RemoveTail () =
-        this :> ICList<'A>
+        let rec rt n =
+          match n with
+          | Empty -> Empty
+          | N(vl, N(_, Empty)) -> N(vl, Empty)
+          | N(vl, nxt) -> N(vl, rt nxt)
+        CListAdt(rt head, max (length - 1) 0) :> ICList<'A>
       member this.RemoveAt i =
-        this :> ICList<'A>
+        if i >= length then
+          this :> ICList<'A>
+        elif i = 0 then
+          (this :> ICList<'A>).RemoveHead()
+        elif i = length - 1 then
+          (this :> ICList<'A>).RemoveTail()
+        else
+          let rec ra n index =
+            match n with
+            | N(vl, (N(_, nxt) as n')) ->
+              if index = i - 1 then
+                N(vl, nxt)
+              else
+                N(vl, ra n' (index + 1))
+            | _ -> Empty
+          CListAdt<'A>(ra head 0, length - 1) :> ICList<'A>
       member this.Find func =
-        Some value
+        let rec fnd n =
+          match n with
+          | Empty -> None
+          | N(vl, nxt) -> if func vl then Some vl else fnd nxt
+        fnd head
       member this.ConcatWith l =
-        this :> ICList<'A>
+        let rec toNode (l : ICList<'A>) =
+          match l.Length() with
+          | 0 -> Empty
+          | _ -> N(l.HeadValue().Value, toNode (l.RemoveHead()))
+        let rec cw n n' =
+          match n with
+          | Empty -> n'
+          | N(v, n'') -> N(v, cw n'' n')
+        CListAdt((cw head (toNode l)), (length + l.Length())) :> ICList<'A>
 
-    static member Tail(l) =
-      let rec t (l : CListAdt<'A>) =
-        match l.GetNext with
-        | None -> l
-        | Some l' -> t l'
-      t l
-    static member FoldBack(f, a, l) =
-      let rec foldBack f a (l : CListAdt<'B>) =
-        match l.GetValue with
-        | Unchecked.defaultof<'B> -> Unchecked.defaultof<'B>
-        | v -> foldBack f (f a v) l.GetNext
-      f a l
+    override this.ToString() =
+      let rec itos n =
+        match n with
+        | Empty -> "]"
+        | N(vl, Empty) -> vl.ToString() + " ]"
+        | N(vl, nxt) -> vl.ToString() + "; " + (itos nxt)
+      "[ " + (itos head)
   end
 
 // 29. Реализация интерфейса списка на массиве
 
 [<EntryPoint>]
 let main argv =
+  printfn "28. Реализация интерфейса списка на АТД. Демонстрация:"
+  let mutable l1 = CListAdt<int>(3) :> ICList<int>;
+  printfn "l1 = %A" l1
+  printfn "> AddHead 1 // a"
+  l1 <- l1.AddHead 1
+  printfn "l1 = %A\n> AddTail 4 // b" l1
+  l1 <- l1.AddTail 4
+  printfn "l1 = %A\n> AddBefore 2 1 // c" l1
+  l1 <- l1.AddBefore 2 1
+  printfn "l1 = %A\n> AddBefore 0 0" l1
+  l1 <- l1.AddBefore 0 0
+  printfn "l1 = %A\n> AddBefore 5 6" l1
+  l1 <- l1.AddBefore 5 6
+  printfn "l1 = %A\n> RemoveHead() // d" l1
+  l1 <- l1.RemoveHead()
+  printfn "l1 = %A\n> RemoveTail() // e" l1
+  l1 <- l1.RemoveTail()
+  printfn "l1 = %A\n> RemoveAt 1 // f" l1
+  l1 <- l1.RemoveAt 1
+  printfn "l1 = %A\n> let mutable l2 = l1.RemoveAt 3" l1
+  let mutable l2 = l1.RemoveAt 3
+  printfn "l2 = %A" l2
+  printfn ">l2.Find ((=) 3) // g\n%A"  (l2.Find ((=) 3))
+  printfn "> l1 <- l1.ConcatWith l2 // h"
+  l1 <- l1.ConcatWith l2
+  printfn "l1 = %A\n" l1
+
   0
