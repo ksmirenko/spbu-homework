f8a5e96ff5a0d0b28a476e0bfd907736ce6359c5
Kirill Smirenko <ksmirenko@mail.ru>
2015-03-23 12:26:36 +0300

    Исправлены задания 27-29

--- 15-03-16/part3.fs
+++ 15-03-16/part3.fs
@@ -3,19 +3,22 @@
 // Расчётное время выполнения: 2 часа
 // Действительное время выполнения: 3 часа
 
+// Исправленная версия: методы теперь возвращают bool или unit
+
 // 27. Интерфейс для полиморфного списка
 type ICList<'A when 'A : equality> =
   interface
     abstract HeadValue   : Option<'A> with get
     abstract Length      : int with get
-    abstract AddHead     : 'A            -> ICList<'A>
-    abstract AddTail     : 'A            -> ICList<'A>
-    abstract AddBefore   : 'A            -> int         -> ICList<'A>
-    abstract RemoveHead  : unit          -> ICList<'A>
-    abstract RemoveTail  : unit          -> ICList<'A>
-    abstract RemoveAt    : int           -> ICList<'A>
+    abstract Tail        : ICList<'A> with get
+    abstract AddHead     : 'A            -> unit
+    abstract AddTail     : 'A            -> unit
+    abstract AddBefore   : 'A            -> int         -> bool
+    abstract RemoveHead  : unit          -> bool
+    abstract RemoveTail  : unit          -> bool
+    abstract RemoveAt    : int           -> bool
     abstract Find        : ('A -> bool)  -> Option<'A>
-    abstract ConcatWith  : ICList<'A>    -> ICList<'A>
+    abstract Append  : ICList<'A>    -> unit
   end
 
 type Node<'A> =
@@ -25,8 +28,8 @@ type Node<'A> =
 // 28. Реализация интерфейса списка на АТД
 type CListAdt<'A when 'A : equality>(h : Node<'A>, s : int) =
   class
-    let head = h
-    let length = s
+    let mutable head = h
+    let mutable length = s
 
     let rec tail (l : Node<'A>) =
       match l with
@@ -46,17 +49,24 @@ type CListAdt<'A when 'A : equality>(h : Node<'A>, s : int) =
           | Empty -> None
           | N(v, _) -> Some v
       member this.Length with get () = length
+      member this.Tail
+        with get () =
+          match head with
+          | Empty -> (new CListAdt<'A>()) :> ICList<'A>
+          | N(_, nxt) -> (new CListAdt<'A>(nxt, length - 1)) :> ICList<'A>
       member this.AddHead v =
-        new CListAdt<'A>(N(v, head), length + 1) :> ICList<'A>
+        head <- N(v, head)
+        length <- length + 1
       member this.AddTail v =
         let rec at n v =
           match n with
           | Empty -> N(v, Empty)
           | N(v', n') -> N(v', at n' v)
-        CListAdt(at head v, length + 1) :> ICList<'A>
+        head <- at head v
+        length <- length + 1
       member this.AddBefore v i =
         if (i < 0) || (i >= length) then
-          failwith "Index out of range!"
+          false
         else
           let rec ab node v index =
             match node with
@@ -65,25 +75,37 @@ type CListAdt<'A when 'A : equality>(h : Node<'A>, s : int) =
               match compare index i with
               | c when c < 0 -> N(vl, ab nxt v (index + 1))
               | _ -> N(v, node)
-          CListAdt(ab head v 0, length + 1) :> ICList<'A>
+          head <- ab head v 0
+          length <- length + 1
+          true
       member this.RemoveHead () =
         match head with
-        | Empty -> this :> ICList<'A>
-        | N(vl, nxt) -> CListAdt<'A>(nxt, length - 1) :> ICList<'A>
+        | Empty -> false
+        | N(vl, nxt) ->
+          head <- nxt
+          length <- length - 1
+          true
       member this.RemoveTail () =
-        let rec rt n =
-          match n with
-          | Empty -> Empty
-          | N(vl, N(_, Empty)) -> N(vl, Empty)
-          | N(vl, nxt) -> N(vl, rt nxt)
-        CListAdt(rt head, max (length - 1) 0) :> ICList<'A>
+        if length = 0 then
+          false
+        else
+          let rec rt n =
+            match n with
+            | Empty -> Empty
+            | N(vl, N(_, Empty)) -> N(vl, Empty)
+            | N(vl, nxt) -> N(vl, rt nxt)
+          head <- rt head
+          length <- length - 1
+          true
       member this.RemoveAt i =
         if (i < 0) || (i >= length) then
-          failwith "Index out of range!"
+          false
         elif i = 0 then
-          (this :> ICList<'A>).RemoveHead()
+          ignore ((this :> ICList<'A>).RemoveHead())
+          true
         elif i = length - 1 then
-          (this :> ICList<'A>).RemoveTail()
+          ignore ((this :> ICList<'A>).RemoveTail())
+          true
         else
           let rec ra n index =
             match n with
@@ -93,23 +115,26 @@ type CListAdt<'A when 'A : equality>(h : Node<'A>, s : int) =
               else
                 N(vl, ra n' (index + 1))
             | _ -> Empty
-          CListAdt<'A>(ra head 0, length - 1) :> ICList<'A>
+          head <- ra head 0
+          length <- length - 1
+          true
       member this.Find func =
         let rec fnd n =
           match n with
           | Empty -> None
           | N(vl, nxt) -> if func vl then Some vl else fnd nxt
         fnd head
-      member this.ConcatWith l =
+      member this.Append l =
         let rec toNode (l : ICList<'A>) =
           match l.Length with
           | 0 -> Empty
-          | _ -> N(l.HeadValue.Value, toNode (l.RemoveHead()))
+          | _ -> N(l.HeadValue.Value, toNode (l.Tail))
         let rec cw n n' =
           match n with
           | Empty -> n'
           | N(v, n'') -> N(v, cw n'' n')
-        CListAdt((cw head (toNode l)), (length + l.Length)) :> ICList<'A>
+        head <- cw head (toNode l)
+        length <- length + l.Length
 
     override this.ToString() =
       let rec itos n =
@@ -123,11 +148,10 @@ type CListAdt<'A when 'A : equality>(h : Node<'A>, s : int) =
 // 29. Реализация интерфейса списка на массиве
 type CListArray<'A when 'A : equality>(arr : 'A []) =
   class
-    let array = arr
-    let length = array.Length
+    let mutable array = arr
 
-    let wrap v =
-      Array.create 1 v
+    let length () = array.Length
+    let wrap v = Array.create 1 v
 
     new() = CListArray<'A>(Array.empty<'A>)
     new(v : 'A) = CListArray<'A>(Array.create 1 v)
@@ -138,47 +162,56 @@ type CListArray<'A when 'A : equality>(arr : 'A []) =
           match array with
           | [||] -> None
           | _ -> Some array.[0]
-      member this.Length with get () = length
+      member this.Length with get () = length()
+      member this.Tail
+        with get () =
+          match length() with
+          | 0 -> this :> ICList<'A>
+          | _ -> CListArray<'A>(Array.sub array 1 (length() - 1)) :> ICList<'A>
       member this.AddHead v =
-        CListArray<'A>(Array.append (wrap v) array) :> ICList<'A>
+        array <- Array.append (wrap v) array
       member this.AddTail v =
-        CListArray<'A>(Array.append array (wrap v)) :> ICList<'A>
+        array <- Array.append array (wrap v)
       member this.AddBefore v i =
-        if (i < 0) || (i >= length) then
-          failwith "Index out of range!"
+        if (i < 0) || (i >= length()) then
+          false
         else
-          let a =
+          array <-
             Array.append
               (Array.append (Array.sub array 0 i) (wrap v))
-                (Array.sub array i (length - i + 1))
-          CListArray<'A>(a) :> ICList<'A>
+                (Array.sub array i (length() - i + 1))
+          true
       member this.RemoveHead () =
-        match length with
-        | 0 -> this :> ICList<'A>
-        | _ -> CListArray<'A>(Array.sub array 1 (length - 1)) :> ICList<'A>
+        match length() with
+        | 0 -> false
+        | _ ->
+          array <- Array.sub array 1 (length() - 1)
+          true
       member this.RemoveTail () =
-        match length with
-        | 0 -> this :> ICList<'A>
-        | _ -> CListArray<'A>(Array.sub array 0 (length - 1)) :> ICList<'A>
+        match length() with
+        | 0 -> false
+        | _ ->
+          array <- Array.sub array 0 (length() - 1)
+          true
       member this.RemoveAt i =
-        if (i < 0) || (i >= length) then
-          failwith "Index out of range!"
+        if (i < 0) || (i >= length()) then
+          false
         else
-          match length with
-          | 0 -> this :> ICList<'A>
+          match length() with
+          | 0 -> false
           | _ ->
-            let a =
+            array <-
               Array.append (Array.sub array 0 (i - 1)) 
-                (Array.sub array i (length - i))
-            CListArray<'A>(a) :> ICList<'A>
+                (Array.sub array i (length() - i))
+            true
       member this.Find func =
         Array.tryFind func array
-      member this.ConcatWith l =
+      member this.Append l =
         let rec toArr (l : ICList<'A>) =
           match l.Length with
           | 0 -> [||]
-          | _ -> Array.append (wrap l.HeadValue.Value) (toArr (l.RemoveHead()))
-        CListArray<'A>(Array.append array (toArr l)) :> ICList<'A>
+          | _ -> Array.append (wrap l.HeadValue.Value) (toArr (l.Tail))
+        array <- Array.append array (toArr l)
 
     override this.ToString() =
       array.ToString()
@@ -187,57 +220,57 @@ type CListArray<'A when 'A : equality>(arr : 'A []) =
 [<EntryPoint>]
 let main argv =
   printfn "28. Реализация интерфейса списка на АТД:"
-  let mutable l1 = CListAdt<int>(3) :> ICList<int>;
+  let l1 = CListAdt<int>(3) :> ICList<int>;
   printfn "l1 = %A" l1
   printfn "// a\n> AddHead 1"
-  l1 <- l1.AddHead 1
-  printfn "l1 = %A\n// b\n> AddTail 4" l1
-  l1 <- l1.AddTail 4
-  printfn "l1 = %A\n// c\n> AddBefore 2 1" l1
-  l1 <- l1.AddBefore 2 1
-  printfn "l1 = %A\n> AddBefore 0 0" l1
-  l1 <- l1.AddBefore 0 0
-  printfn "l1 = %A\n> AddTail 5" l1
-  l1 <- l1.AddTail 5
-  printfn "l1 = %A\n// d\n> RemoveHead()" l1
-  l1 <- l1.RemoveHead()
-  printfn "l1 = %A\n// e\n> RemoveTail()" l1
-  l1 <- l1.RemoveTail()
-  printfn "l1 = %A\n// f\n> RemoveAt 1" l1
-  l1 <- l1.RemoveAt 1
-  printfn "l1 = %A\n> let mutable l1' = l1.RemoveAt 2" l1
-  let mutable l1' = l1.RemoveAt 2
+  l1.AddHead 1
+  printfn "l1 = %A\n// b\n> l1.AddTail 4" l1
+  l1.AddTail 4
+  printfn "l1 = %A\n// c\n> l1.AddBefore 2 1" l1
+  ignore (l1.AddBefore 2 1)
+  printfn "l1 = %A\n> l1.AddBefore 0 0" l1
+  ignore (l1.AddBefore 0 0)
+  printfn "l1 = %A\n> l1.AddTail 5" l1
+  l1.AddTail 5
+  printfn "l1 = %A\n// d\n> l1.RemoveHead()" l1
+  ignore (l1.RemoveHead())
+  printfn "l1 = %A\n// e\n> l1.RemoveTail()" l1
+  ignore (l1.RemoveTail())
+  printfn "l1 = %A\n// f\n> l1.RemoveAt 1" l1
+  ignore (l1.RemoveAt 1)
+  printfn "l1 = %A\n> let l1' = l1.Tail" l1
+  let l1' = l1.Tail
   printfn "l1' = %A" l1'
   printfn "// g\n> l1'.Find ((=) 3)\n%A"  (l1'.Find ((=) 3))
-  printfn "// h\n> l1 <- l1.ConcatWith l1'"
-  l1 <- l1.ConcatWith l1'
+  printfn "// h\n> l1.Append l1'"
+  l1.Append l1'
   printfn "l1 = %A\n" l1
 
   printfn "29. Реализация интерфейса списка на массиве:"
-  let mutable l2 = CListAdt<int>(3) :> ICList<int>;
+  let l2 = CListAdt<int>(3) :> ICList<int>;
   printfn "l2 = %A" l2
   printfn "// a\n> AddHead 1"
-  l2 <- l2.AddHead 1
-  printfn "l2 = %A\n// b\n> AddTail 4" l2
-  l2 <- l2.AddTail 4
-  printfn "l2 = %A\n// c\n> AddBefore 2 1" l2
-  l2 <- l2.AddBefore 2 1
-  printfn "l2 = %A\n> AddBefore 0 0" l2
-  l2 <- l2.AddBefore 0 0
-  printfn "l2 = %A\n> AddTail 5" l2
-  l2 <- l2.AddTail 5
-  printfn "l2 = %A\n// d\n> RemoveHead()" l2
-  l2 <- l2.RemoveHead()
-  printfn "l2 = %A\n// e\n> RemoveTail()" l2
-  l2 <- l2.RemoveTail()
-  printfn "l2 = %A\n// f\n> RemoveAt 1" l2
-  l2 <- l2.RemoveAt 1
-  printfn "l2 = %A\n> let mutable l2' = l2.RemoveAt 2" l2
-  let mutable l2' = l2.RemoveAt 2
+  l2.AddHead 1
+  printfn "l2 = %A\n// b\n> l2.AddTail 4" l2
+  l2.AddTail 4
+  printfn "l2 = %A\n// c\n> l2.AddBefore 2 1" l2
+  ignore (l2.AddBefore 2 1)
+  printfn "l2 = %A\n> l2.AddBefore 0 0" l2
+  ignore (l2.AddBefore 0 0)
+  printfn "l2 = %A\n> l2.AddTail 5" l2
+  l2.AddTail 5
+  printfn "l2 = %A\n// d\n> l2.RemoveHead()" l2
+  ignore (l2.RemoveHead())
+  printfn "l2 = %A\n// e\n> l2.RemoveTail()" l2
+  ignore (l2.RemoveTail())
+  printfn "l2 = %A\n// f\n> l2.RemoveAt 1" l2
+  ignore (l2.RemoveAt 1)
+  printfn "l2 = %A\n> let l2' = l2.Tail" l2
+  let l2' = l2.Tail
   printfn "l2' = %A" l2'
   printfn "// g\n> l2'.Find ((=) 3)\n%A"  (l2'.Find ((=) 3))
-  printfn "// h\n> l2 <- l2.ConcatWith l2'"
-  l2 <- l2.ConcatWith l2'
+  printfn "// h\n> l2.Append l2'"
+  l2.Append l2'
   printfn "l2 = %A\n" l2
 
   0
